461f7d22fce45b574b5296d608bd2143ab54338f
jknndti <jknn@dti.dk>
2021-05-22 17:22:06 +0200

    test may 2021

--- js/script.js
+++ js/script.js
@@ -2,5 +2,23 @@
 
 function sayHello() {
 console.log(document.getElementById("name").value);	// body...
-console.log(document.getElementById("name").value);
+//console.log(document.getElementById("name").value + "dd");
+console.log(222);
 };
+
+var facebook = {
+	name: "facebook",
+	ceo: {
+		FirstName: "Mark"
+	}
+}; 
+
+// console.log(facebook.ceo.FirstName)
+
+function multiply(x, y) {
+	return x*y;
+}
+
+//console.log(multiply(5, 3)); 
+multiply.version = "v.1"; 
+console.log(multiply.version); 
